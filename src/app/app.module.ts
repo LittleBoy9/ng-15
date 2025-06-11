55f16aa94e55f70df5f6b4fbaaa44f7365ab4af0
Sounak <sounak.das@vitra.ai>
2025-06-11 14:33:45 +0530

    feat: added topbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { TopbarComponent } from './shared/topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
